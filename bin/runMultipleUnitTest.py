d85a9446baee717547174c03ce201bd956b9b145
Michael Wetter <mwetter@lbl.gov>
2021-12-03 07:36:52 -0800

    Updated run script

--- bin/runMultipleUnitTest.py
+++ bin/runMultipleUnitTest.py
@@ -3,5 +3,5 @@ set -e # This will stop when diff returns non-zero
 
 for i in {1..100}; do
   echo "******* i = $i"
-  ../bin/runUnitTests.py -s Buildings.ThermalZones.EnergyPlus.Examples -b -d -n 1
+  ../bin/runUnitTests.py -s Buildings.Examples.VAVReheat -b
 done
