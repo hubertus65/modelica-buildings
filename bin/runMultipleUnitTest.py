2400ffb246693e48f8ac037e09aecb50481db374
Michael Wetter <mwetter@lbl.gov>
2021-12-08 04:46:02 -0800

    Changed to use only 1 processor

--- bin/runMultipleUnitTest.py
+++ bin/runMultipleUnitTest.py
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e # This will stop when diff returns non-zero
 
-for i in {1..100}; do
+for i in {1..500}; do
   echo "******* i = $i"
-  ../bin/runUnitTests.py -s Buildings.Examples.VAVReheat -b -d
+  ../bin/runUnitTests.py -s Buildings.Examples.VAVReheat -b -d -n 1
   rm -rf ../tmp-Buildings*
   mv /tmp/tmp-Buildings* ..
 done
